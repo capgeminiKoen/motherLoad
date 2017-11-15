34d652dd1de033acec8e2df47c44ebc719254670
capgeminiKoen <33454421+capgeminiKoen@users.noreply.github.com>
2017-11-15 13:44:05 +0100

    updated inventoryScreen

--- core/src/com/mygdx/game/GUI/Hud.java
+++ core/src/com/mygdx/game/GUI/Hud.java
@@ -3,8 +3,6 @@ package com.mygdx.game.GUI;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.game.Coordinate;
--- core/src/com/mygdx/game/GUI/InventoryScreen.java
+++ core/src/com/mygdx/game/GUI/InventoryScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.game.GUI;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.game.Manager;
 import com.mygdx.game.inventory.Inventory;
 import com.mygdx.game.inventory.resources.Resource;
@@ -20,6 +21,12 @@ public class InventoryScreen extends Screen {
 
     @Override
     public void draw(SpriteBatch batch) {
+        // Draw background
+        drawBackground(batch);
+
+        // Draw grid
+        drawInventoryFrame(batch, Manager.screenSize.x - width, 0);
+
         // Set projection matrix to the camera's position
         batch.setProjectionMatrix(Manager.camera.projection);
         // Draw title
@@ -30,6 +37,27 @@ public class InventoryScreen extends Screen {
 
         // Set original matrix back again
         batch.setProjectionMatrix(Manager.camera.combined);
+    }
+
+    private void drawBackground(SpriteBatch batch){
+        // Skip spriteBatch right now
+        batch.end();
+
+        // Draw the hit
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setAutoShapeType(true);
+        // Bar itself
+        Color color = new Color(0.4f,0.4f,0.4f,1);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(color);
+
+        // Draw rect
+        shapeRenderer.rect(Manager.screenSize.x - width, 0, width, height);
+
+        shapeRenderer.end();
+
+        // Start batch again
+        batch.begin();
     }
 
     private void drawInventoryItems(SpriteBatch batch, float x, float y){
@@ -43,5 +71,32 @@ public class InventoryScreen extends Screen {
                 currentIndex++;
             }
         }
+    }
+
+    private void drawInventoryFrame(SpriteBatch batch, float x, float y){
+        // Skip spriteBatch right now
+        batch.end();
+
+        // Draw the hit
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setAutoShapeType(true);
+        // Bar itself
+        Color color = new Color(0.2f,0.2f,0.2f,1);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(color);
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                shapeRenderer.rect(x + j * tileSize, y + i * tileSize, tileSize, tileSize);
+            }
+        }
+
+        shapeRenderer.end();
+
+        // Start batch again
+        batch.begin();
+
+
     }
 }
